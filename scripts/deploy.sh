dcaf3591041e9852ba9902ef133d9590495da005
Nathan Smith <nathn.smith@gmail.com>
2018-06-13 20:58:47 +1000

    Messaging

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,8 +9,10 @@ forge deploy \
     --template-file cloudformation/vpn.yaml \
     --parameters-file params/params.yaml
 
-echo "Username is: $UNAME"
-echo "Password is: $PWD"
-echo "Passphrase is: $PHRASE"
+echo "Please give the server ~5 minutes to fully configure"
+echo "Ensure you send all traffic to the VPN to aid anonymity"
+echo "Username: $UNAME"
+echo "Password: $PWD"
+echo "Passphrase: $PHRASE"
 
 aws cloudformation describe-stacks --query "Stacks[?StackName=='$STACK_NAME'].Outputs[*]"
