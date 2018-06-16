fa73dbec97da08ca35c37e1bf7125e58a2029ece
Nathan Smith <nathn.smith@gmail.com>
2018-06-16 13:11:06 +1000

    Consistent naming and additional messaging

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,10 +9,13 @@ forge deploy \
     --template-file cloudformation/vpn.yaml \
     --parameters-file params/params.yaml
 
+echo
 echo "Please give the server ~5 minutes to fully configure"
 echo "Ensure you send all traffic to the VPN to aid anonymity"
-echo "Username: $UNAME"
-echo "Password: $PWD"
-echo "Passphrase: $PHRASE"
+echo "For max security use HTTPS DNS 1.1.1.1 in your settings"
+echo
 
+echo "Username: $USERNAME"
+echo "Password: $PASSWORD"
+echo "Passphrase: $PHRASE"
 aws cloudformation describe-stacks --query "Stacks[?StackName=='$STACK_NAME'].Outputs[*]"
--- scripts/stack_name_vars.sh
+++ scripts/stack_name_vars.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-export UNAME=$USER
-export STACK_NAME=$UNAME-vpn
-export PWD="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13)"
+export USERNAME=$USER
+export STACK_NAME=$USERNAME-vpn
+export PASSWORD="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13)"
 export PHRASE="$(head /dev/urandom | tr -dc A-Za-z0-9 | head -c 13)"
