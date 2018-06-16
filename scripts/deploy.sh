af88ceadd423927d3ee23c717232c35f61937060
Nathan Smith <nathn.smith@gmail.com>
2018-06-16 13:21:15 +1000

    Tidy up address output

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -9,13 +9,17 @@ forge deploy \
     --template-file cloudformation/vpn.yaml \
     --parameters-file params/params.yaml
 
+ADDRESS=$(aws cloudformation describe-stacks --query "Stacks[?StackName=='$STACK_NAME'].Outputs[0].OutputValue" --output text)
+
 echo
 echo "Please give the server ~5 minutes to fully configure"
 echo "Ensure you send all traffic to the VPN to aid anonymity"
 echo "For max security use HTTPS DNS 1.1.1.1 in your settings"
 echo
 
+echo "--------------------"
+echo "Address: $ADDRESS"
 echo "Username: $USERNAME"
 echo "Password: $PASSWORD"
 echo "Passphrase: $PHRASE"
-aws cloudformation describe-stacks --query "Stacks[?StackName=='$STACK_NAME'].Outputs[*]"
+echo "--------------------"
